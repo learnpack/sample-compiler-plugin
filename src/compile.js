adec97b422a152bac02ee5a24f72e6180ef74784
Alejandro Sanchez <aalejo@gmail.com>
2021-07-25 01:49:41 +0000

    better documentation

--- src/compile.js
+++ src/compile.js
@@ -22,39 +22,24 @@ const { Utils, CompilationError } = require('learnpack/plugin')
 
  */
 module.exports = {
-  validate: () => true,
+  validate: () => {
+
+    // return true or false if the compilation should take place
+    // you can leave this empty if you don't know what to validate.
+
+    return true
+  },
   run: async function ({ exercise, socket, configuration }) {
 
-    let entryPath = exercise.entry || exercise.files.map(f => './'+f.path).find(f => f.indexOf('index.html') > -1)
-    if(!entryPath) throw CompilationError("Missing index.html entry file");
+    // here you should include your compulation code, the files to compile will be inside exercise.files
 
-    let errors = exercise.files.filter(f => f.path.includes(".html") || f.path.includes(".css")).map((file)=>{
-      const prettyConfig = {
-          parser: file.name.split('.').pop(),
-          printWidth: 150,             // Specify the length of line that the printer will wrap on.
-          tabWidth: 4,                // Specify the number of spaces per indentation-level.
-          useTabs: true,              // Indent lines with tabs instead of spaces.
-          bracketSpacing: true,
-          semi: true,                 // Print semicolons at the ends of statements.
-          encoding: 'utf-8'           // Which encoding scheme to use on files
-      };
-      const content = fs.readFileSync(file.path, "utf8")
-      try{
-        const formatted = prettier.format(content, prettyConfig)
-        fs.writeFileSync(file.path, formatted)
-        fs.writeFileSync(`${configuration.outputPath}/${file.name}`, formatted)
-      }
-      catch(error){
-        return error;
-      }
-      return null;
-    });
-
-    const foundErrors = [].concat(errors.filter(e => e !== null))
+    const errors = []
     if(foundErrors.length > 0) throw CompilationError(foundErrors.map(e => e.message).join(""))
     
+    // if you need to open the compiler website on a new window
     socket.openWindow(`${configuration.publicUrl}/preview`)
 
+    // return string with the console output (stdout)
     return Utils.cleanStdout("Successfully built your HTML")
   },
 }
--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,13 @@
 const { plugin } = require("learnpack/plugin")
 
+/**
+ * This is the beginning of your project, there are three things to define here:
+ * 
+ * language:    Its important to specify what language is your package going to be compiling; 
+ *              Some of the possible languages are html, css, react, java, node and python.
+ * compile:     object that contains two properties functions: run and validate.
+ * test:        object that contains two properties functions: run and validate.
+ */
 module.exports = plugin({
     language: "html",
     compile: require('./compile'),
